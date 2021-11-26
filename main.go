dd0c0bbbd552bf3faed12fcce804b6685bb122e1
lixin10 <lixin10@bilibili.com>
2021-11-26 16:37:34 +0800

    add one path

--- main.go
+++ main.go
@@ -15,5 +15,10 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/testing", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
